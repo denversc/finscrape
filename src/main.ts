f3ff9064821d766efa628ddd7b89217dbeda0dd6
Denver Coneybeare <github.prefix924@passmail.com>
2025-05-18 01:21:24 -0400

    crypto.ts added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,32 @@
 import puppeteer from "puppeteer";
+import yargs from 'yargs';
+import { hideBin } from "yargs/helpers";
 
 import * as td from "./td/td.ts";
 import { getLogger } from "./util/logging.ts";
 import { PuppeteerHelper } from "./util/puppeteer_helper.ts";
 
-const leaveBrowserOpen = true;
+const yargsResult = await yargs(hideBin(process.argv))
+  .usage('$0 [options]')
+  .option('username', {
+    string: true,
+    description: 'The username for logging into the web site.',
+  })
+  .option('encrypted-password', {
+    string: true,
+    description: 'The (encrypted) password for logging into the web site.',
+  })
+  .option('leave-browser-open', {
+    boolean: true,
+    description: 'Leave the browser opened instead of closing it at the end ' +
+    '(useful for debugging)',
+  })
+  .showHelpOnFail(true)
+  .strict()
+  .parse();
+
+const {username, password, leaveBrowserOpen} = yargsResult;
+
 const logger = getLogger();
 const browser = await puppeteer.launch({ headless: false });
 const puppeteerHelper = new PuppeteerHelper(logger);
---
+++ src/util/crypto.ts
@@ -0,0 +1,35 @@
+import crypto from "node:crypto";
+
+const key = '1ErDLz9W8fJCQ+gGiKICBPBfy5TkBL9Dx84nAF6Hkvc=' as const;
+const iv = '/PqhiCThqD3Lli1g6ceVqw==' as const;
+const cipherType = 'aes-256-gcm' as const;
+
+interface EncryptedData {
+  cipherText: string;
+  authTag: string;
+}
+
+export function encrypt(text: string): string {
+  const cipher = crypto.createCipheriv(cipherType, fromBase64(key), fromBase64(iv));
+  const cipherText1 = cipher.update(text, "utf8", "base64");
+  const cipherText2 = cipher.final("base64");
+  const cipherText = cipherText1 + cipherText2;
+  const authTag = cipher.getAuthTag().toString("base64");
+
+  const encryptedData: EncryptedData = { cipherText, authTag };
+  return Buffer.from(new TextEncoder().encode(JSON.stringify(encryptedData))).toString("base64");
+}
+
+export function decrypt(encryptedText: string): string {
+  const encryptedData: EncryptedData = JSON.parse(new TextDecoder().decode(Buffer.from(encryptedText, "base64")));
+  const { cipherText, authTag } = encryptedData;
+  const decipher = crypto.createDecipheriv(cipherType, fromBase64(key), fromBase64(iv));
+  decipher.setAuthTag(Buffer.from(authTag, "base64"));
+  const plainText1 = decipher.update(cipherText, "base64", "utf8");
+  const plainText2 = decipher.final("utf8");
+  return plainText1 + plainText2;
+}
+
+function fromBase64(text: string): Uint8Array {
+  return new Uint8Array(Buffer.from(text, "base64"));
+}
